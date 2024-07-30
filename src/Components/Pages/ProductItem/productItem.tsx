bbd76cd2e60abb96ed11fce66abd6a1c08cb6816
Ivan-ISS <vanya5@inbox.ru>
2024-07-31 01:20:12 +0500

    project_dm-tech: added a back button on the single product page

--- src/Components/Pages/ProductItem/productItem.tsx
+++ src/Components/Pages/ProductItem/productItem.tsx
@@ -1,15 +1,19 @@
 import * as styles from './productItem.module.scss';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '@/redux/store';
 import { fetchProduct } from '@/redux/slices/productItemSlice/productItemSlice';
 import { selectItemProduct } from '@/redux/slices/productItemSlice/productSliceSelector';
 import ProductDetailedCard from '../../ProductDetailedCard/productDetailedCard';
+import ArrowLeft from '@/assets/images/svg/arrowLeft.svg';
+import Item from '../../Common/Item/item';
+import routes from '@/routes';
 
 export default function ProductItem() {
     const dispatch = useAppDispatch();
     const product = useAppSelector(selectItemProduct);
     const { id } = useParams();
+    const navigate = useNavigate();
     // const product = products.find(product => product.id === id);
 
     useEffect(() => {
@@ -18,6 +22,10 @@ export default function ProductItem() {
 
     return (
         <section className={styles.productItem}>
+            <button className={styles.backButton} onClick={() => navigate(routes.products())}>
+                <ArrowLeft width={20} height={20}/>
+                <Item text={'Назад'}/>
+            </button>
             <ProductDetailedCard product={product}/>
         </section>
     );
